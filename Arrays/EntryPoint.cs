4c13c27b155080c66518802d3ec582802cfc0062
paulfromeq3 <paul.walker@eq3.org>
2021-08-09 11:47:58 -0500

    Creating an array

--- Arrays/EntryPoint.cs
+++ Arrays/EntryPoint.cs
@@ -4,6 +4,26 @@ class EntryPoint
 {
     static void Main()
     {
+        // An array is basically a container for a set amount of values.
+        // You can have arrays of different types
+        // Access any value in an array using its index.
+        // Indices always start at zero.
+
+        // Create an array of temperatures
+        // First approach: create an empty array of a set number of values.
+        double[] temperatures = new double[5];
+        temperatures[0] = 99.0;
+        temperatures[1] = 110.2;
+        temperatures[2] = 107.4;
+        temperatures[3] = 78.0;
+        temperatures[4] = 88.1;
+        Console.WriteLine(temperatures[2]);
+
+        // Second approach: assign values when you create the array.
+        double[] temperaturesUK = { 32.0, 33.0, 43.0, 22.0, 35.4 };
+        Console.WriteLine(temperaturesUK[2]);
+
+
 
     }
 }
